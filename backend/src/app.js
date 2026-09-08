c61651402e0ada149ddee0431f55a15c65893f09
ADMIN <josh.dave.bengaura@gmail.com>
2026-09-08 20:53:41 +0300

    Initial setup for backend. frontend to be initialized soon

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import router from "./api/index.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
+app.use("/public", express.static("public"));
