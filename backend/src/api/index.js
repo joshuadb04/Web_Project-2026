a65e6eb82d0a625389c25986bd29a59eede3c051
ADMIN <josh.dave.bengaura@gmail.com>
2026-09-16 13:18:47 +0300

    test

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-//import promisePool from "../utils/database.js";
+import promisePool from "../utils/database.js";
 const router = express.Router();
-//const test = async () => {
-//  const result = await promisePool.query("SELECT * FROM users");
-//  console.log(result);
-//};
-//test();
+const test = async () => {
+  const result = await promisePool.query("SELECT * FROM users");
+  console.log(result);
+};
+test();
 export default router;
