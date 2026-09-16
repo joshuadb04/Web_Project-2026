97a6b27aaf55a07a8dfa207c38cacb85f6f616ac
ADMIN <josh.dave.bengaura@gmail.com>
2026-09-16 13:17:41 +0300

    small test made by joshua

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -2,8 +2,8 @@ import express from "express";
 //import promisePool from "../utils/database.js";
 const router = express.Router();
 //const test = async () => {
-// const result = await promisePool.query("SELECT * FROM users");
-// console.log(result);
+//  const result = await promisePool.query("SELECT * FROM users");
+//  console.log(result);
 //};
 //test();
 export default router;
